39033dbb57a154f526aeb1ae79a685e76bba1079
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2023-07-05 21:04:16 +0900

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # verb " /ask", to:"controller#action"
+
+  get 'ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
+
 end
